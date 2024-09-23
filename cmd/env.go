c4e634fc695ff7f6fcb51b1e86b0e1d8bb34ab29
Leonardo Borges Avelino <lborgav@gmail.com>
2024-09-22 21:31:00 -0300

    chore: cleaning some Cobra placeholders

--- cmd/env.go
+++ cmd/env.go
@@ -54,14 +54,4 @@ These encrypted secrets will be securely stored and used by your application dur
 
 func init() {
 	launchCmd.AddCommand(envCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// envCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// envCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/init.go
+++ cmd/init.go
@@ -159,14 +159,4 @@ var initCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(initCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
